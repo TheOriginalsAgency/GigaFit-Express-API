cd80072c5b93761ae8c8c620da56332ac4110e73
abdessamadoriginals <Abdessamad.theoriginals@gmail.com>
2022-04-29 11:12:32 +0200

    count of all of all v2

--- routes/reservation.route.js
+++ routes/reservation.route.js
@@ -1,9 +1,5 @@
 const express = require('express');
-<<<<<<< HEAD
-const { allReservationByUser, addReservation, annulerReservation, EventIsReserved , allReservation} = require('../controllers/Reservation.controller')
-=======
 const { allReservationByUser, addReservation, annulerReservation, EventIsReserved, allReservation } = require('../controllers/Reservation.controller')
->>>>>>> a7d209dd57f638ab543fbe2dff0ec4ed7e176a29
 const router = express.Router()
 
 // Reservation Methods
