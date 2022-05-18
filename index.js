5779d845fdedbe3218d5173d1fe5f795c229f906
Mohammed1Dev <bettapro100@gmail.com>
2022-05-18 17:30:30 +0000

    Finalisation Admin Panel

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 app.post("/user/forgotPassword/:email", forgetPassword);
 //middleware
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "150kb" }));
 app.use(helmet());
 app.options("*", cors());
 app.use(cors());
